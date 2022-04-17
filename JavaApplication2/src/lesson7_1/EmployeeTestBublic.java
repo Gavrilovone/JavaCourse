a0a994f94786c81e98a88562631adfcaf45a527e
gavrilovone <gavrilovone@mail.ru>
2022-04-17 19:25:51 +0400

    lesson7

---
+++ JavaApplication2/src/lesson7_1/EmployeeTestBublic.java
@@ -0,0 +1,11 @@
+package lesson7_1;
+
+public class EmployeeTestBublic {
+
+    public static void main(String[] args) {
+        lesson7.Employee emp = new lesson7.Employee(500);
+        System.out.println(emp.salary);
+        emp.povyshenyeZP();
+    }
+
+}
