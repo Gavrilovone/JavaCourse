b2138a46e6d378e6a876a28d74bfd1aa858f0ac2
gavrilovone <gavrilovone@mail.ru>
2022-04-20 16:54:14 +0400

    lesson8

---
+++ JavaApplication2/src/lesson8/Car.java
@@ -0,0 +1,21 @@
+
+package lesson8;
+
+public class Car {
+    String color = "blue";
+    String ingine = "V6";
+}
+
+class Human{
+    String name = "Ivan";
+    Car c = new Car();
+    public static void main(String[] args) {
+        Human h1 = new Human();
+        h1.c = new Car();
+        h1.c = new Car();
+        h1.c.color = "red";
+        System.out.println(h1.c.color);
+        
+    }
+}
+    
---
+++ JavaApplication2/src/lesson8/Test1.java
@@ -0,0 +1,43 @@
+package lesson8;
+
+/* - final varibals должны быть проинициализированы до их использования:
+   1. При определении переменной
+   2. Или в каждом конструкторе(если переменная не static) если ststic , то только при создании
+      Если конструкторов несколько, то в каждом она может принимать разные значения.
+   
+   - В каком порядке не имеет значение но обычно первым ставят public
+
+   - У final переменной нет дефолтного значения, поэтому final int a;
+   выдаст ошибку компилятора
+   
+
+
+
+
+ */
+
+public class Test1 {
+    public final int a = 10;
+    final int b;
+    Test1() {
+        b = 10;
+    }
+
+    Test1(Boolean b1) {
+        b = 15;
+    }
+
+    public static void main(String[] args) {
+        Test1 t = new Test1();
+        System.out.println(t.a);
+    }
+    
+    public void abc(final short s){  
+    byte b;
+    b = 10;
+   // s++; будет ошибка так-ак параметр s нельзя изменить внутри метода, так как он final 
+        System.out.println(b+s);
+    }
+    
+
+}
